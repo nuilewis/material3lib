af243540c8f06f5d249d213ab368e0ba6efabe1a
Nuikweh Lewis <72214279+nuilewis@users.noreply.github.com>
2023-07-03 11:54:13 +0100

    Merge CircleCI Configs across branches

---
+++ material3lib/src/main/java/com/nuilewis/material3lib/AddNumbers.java
@@ -0,0 +1,6 @@
+package com.nuilewis.material3lib;
+
+public class AddNumbers {
+
+    public  int addTwoIntergers(int x, int y){return  x+y;}
+}
--- material3lib/src/main/java/com/nuilewis/material3lib/addNumbers.java
+++
@@ -1,6 +0,0 @@
-package com.nuilewis.material3lib;
-
-public class addNumbers {
-
-    public  static int addTwoIntergers(int x, int y){return  x+y;}
-}
